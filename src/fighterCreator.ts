be168db31742eaa53760999842e87aa06f38ef01
Karina Kalwani <alu0101109046@ull.edu.es>
2021-04-12 10:17:15 +0100

    Created FighterCreator abstract class

---
+++ src/fighterCreator.ts
@@ -0,0 +1,13 @@
+import {Fighter} from './fighter';
+
+export abstract class FighterCreator {
+  public abstract factoryMethod(): Fighter;
+
+  public logic(): string {
+    const fighter = this.factoryMethod();
+    return 'I am ${fighter.getName()}, ' +
+           'my weight is ${fighter.getWeight()},' +
+           'my height is ${fighter.getHeight()},' +
+           'and my type is ${fighter.getType()}';
+  }
+}
