1cc9c5a525ac53ee53b9ee2cba1b172860bad601
Karina Kalwani <alu0101109046@ull.edu.es>
2021-04-12 10:57:35 +0100

    Added another test

--- tests/pokemon.spec.ts
+++ tests/pokemon.spec.ts
@@ -6,5 +6,7 @@ describe('testing Pokemon', () => {
   it('A Pokemon should be created succesfully', () => {
     expect(new PokemonCreator('Bulbasaur', 7, 62, 'grass', {attack: 49,
       defense: 49, speed: 45, hp: 45})).not.to.be.null;
+    expect(new PokemonCreator('Blastoise', 7, 62, 'grass', {attack: 49,
+      defense: 49, speed: 45, hp: 45})).not.to.be.null;
   });
 });
